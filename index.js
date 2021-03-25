9e7f8bfc116d339509123e5fb9593e0ee2c3466a
Kherin <kherin.j.s.bundhoo@gmail.com>
2021-03-25 20:12:19 +0400

    feature: annotation

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-try {
+async function run() {
+  try {
+    // inputs
+    const token = core.getInput("token");
+    const annotationTitle = core.getInput("annotation-title");
+    const annotationUrl = core.getInput("annotation-url");
 
-    const annotationTitle = core.getInput('annotation-title');
-    const annotationUrl = core.getInput('annotation-url');
-
-    console.log('annotationTitle: ', annotationTitle);
-    console.log('annotationUrl: ', annotationUrl);
-
+    // pull request details
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload is ${payload}`);
 
-} catch (error) {
+    const { pull_request, repository, number: pull_number } = payload;
+    const pull_name = pull_request["head"]["ref"];
+    const {
+      full_name: repo_pathname,
+      owner: repo_owner,
+      name: repo_name,
+    } = repository;
+    const owner_name = repo_owner["login"];
+
+    // build annotation text
+    const pull_request_path = `${repo_pathname}/tree/${pull_name}`;
+    const annotation_body = `${annotationUrl}${pull_request_path}`;
+
+    const octokit = github.getOctokit(token);
+
+    await octokit.pulls.createReviewComment({
+      owner_name,
+      repo_name,
+      pull_number,
+      annotation_body,
+    });
+
+    console.log(`Annotation - ${annotationTitle} has been successful.`);
+  } catch (error) {
     core.setFailed(error.message);
+  }
 }
+
+run();
