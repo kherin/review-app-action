b9c58d13d37d4dd749bb60c72aa63ac249b2a5c1
Kherin <kherin.j.s.bundhoo@gmail.com>
2021-03-25 23:05:33 +0400

    removed async function

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-async function run() {
-  try {
-    // inputs
-    const token = core.getInput("token");
-    const annotationTitle = core.getInput("annotation-title");
-    const annotationUrl = core.getInput("annotation-url");
+try {
+  // inputs
+  const token = core.getInput("token");
+  const annotationTitle = core.getInput("annotation-title");
+  const annotationUrl = core.getInput("annotation-url");
 
-    // pull request details
-    const { payload } = github.context;
+  // pull request details
+  const { payload } = github.context;
 
-    const { pull_request, repository, number: pull_number } = payload;
-    const pull_name = pull_request["head"]["ref"];
-    const {
-      full_name: repo_pathname,
-      owner: repo_owner,
-      name: repo_name,
-    } = repository;
-    const owner_name = repo_owner["login"];
+  const { pull_request, repository, number: pull_number } = payload;
+  const pull_name = pull_request["head"]["ref"];
+  const {
+    full_name: repo_pathname,
+    owner: repo_owner,
+    name: repo_name,
+  } = repository;
+  const owner_name = repo_owner["login"];
 
-    // build annotation text
-    const pull_request_path = `${repo_pathname}/tree/${pull_name}`;
-    const annotation_body = `${annotationUrl}${pull_request_path}`;
+  // build annotation text
+  const pull_request_path = `${repo_pathname}/tree/${pull_name}`;
+  const annotation_body = `${annotationUrl}${pull_request_path}`;
 
-    const octokit = github.getOctokit(token);
+  const octokit = github.getOctokit(token);
 
-    await octokit.pulls.createReviewComment({
-      owner_name,
-      repo_name,
-      pull_number,
-      annotation_body,
-    });
+  octokit.pulls.createReviewComment({
+    owner_name,
+    repo_name,
+    pull_number,
+    annotation_body,
+  });
 
-    console.log(`Annotation - ${annotationTitle} has been successful.`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  console.log(`Annotation - ${annotationTitle} has been successful.`);
+} catch (error) {
+  core.setFailed(error.message);
 }
-
-run();
